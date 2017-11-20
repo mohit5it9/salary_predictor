9bec6a31e3088834c47ea23ed6520faaae332976
Mohit <mohit@shippable.com>
2017-11-20 21:10:49 +0530

    Update linear_model.py

--- linear_model.py
+++ linear_model.py
@@ -8,7 +8,7 @@ X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:, 1].values
 
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/5, random_state = 0)
 
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
